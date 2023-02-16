11d93703d78f1f19f31e048f0d3f76441acb5ca3
poutingemoji <poutingemoji.dev@gmail.com>
2023-02-15 22:44:37 -0800

    remove katex

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,4 @@
 import "typeface-sriracha"
 import "./static/fonts/wotfard/stylesheet.css"
-import "katex/dist/katex.min.css"
 import { wrapRootElement } from "./src/wrappers/gatsby"
 export { wrapRootElement }
